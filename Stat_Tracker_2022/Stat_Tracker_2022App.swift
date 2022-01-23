898e9e5d07e7cf79db870fe135ac901eb673253b
pottsie <pottsma@me.com>
2022-01-23 07:09:08 -0500

    Initial Commit

---
+++ Stat_Tracker_2022/Stat_Tracker_2022App.swift
@@ -0,0 +1,17 @@
+//
+//  Stat_Tracker_2022App.swift
+//  Stat_Tracker_2022
+//
+//  Created by Michael Potts on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Stat_Tracker_2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
